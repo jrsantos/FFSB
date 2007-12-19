f07712fc3006f0818b7d8ac4dc42410074a6185a
Jose R. Santos <jsantos@gara>
2007-12-19 10:19:14 -0600

    Style changes to ffsb_thread.h
    Whitespace/comment fixes to ffsb_thread.h
    
    Signed-off-by: Jose R. Santos <jrs@us.ibm.com>

--- ffsb_thread.h
+++ ffsb_thread.h
@@ -31,20 +31,24 @@
 struct ffsb_tg;
 struct ffsb_op_results;
 
-/* ffsb thread object */
-/* The thread object doesn't store any configuration information, it mostly */
-/* just holds per-thread state information such as the random data store */
-/* and the per-thread buffer to copy data to/from disk */
+/* FFSB thread object
+ *
+ * The thread object doesn't store any configuration information, it
+ * mostly just holds per-thread state information such as the random
+ * data store and the per-thread buffer to copy data to/from disk
+ */
 
 typedef struct ffsb_thread {
 	unsigned thread_num;
 	unsigned tg_num;
 	pthread_t ptid;
 	struct randdata rd;
-	struct ffsb_tg * tg; /* owning thread group */
+	struct ffsb_tg *tg; /* owning thread group */
 
-/* If we are using Direct IO, then we must only use a 4k aligned buffer*/
-/* so, alignedbuf_4k is a pointer into "mallocbuf" which is what malloc gave us */	
+	/* If we are using Direct IO, then we must only use a 4k
+	 * aligned buffer so, alignedbuf_4k is a pointer into
+	 * "mallocbuf" which is what malloc gave us.
+	 */	
 	char *alignedbuf; 	
 	char *mallocbuf;
 
@@ -52,53 +56,53 @@ typedef struct ffsb_thread {
 	
 	/* stats */
 	ffsb_statsd_t fsd;
-
 } ffsb_thread_t ;
 
-/* */
-void        init_ffsb_thread(ffsb_thread_t *ft,struct ffsb_tg *tg, unsigned bufsize, 
-			     unsigned tg_num, unsigned thread_num); 
-void        destroy_ffsb_thread(ffsb_thread_t *ft);
+void init_ffsb_thread (ffsb_thread_t *, struct ffsb_tg *, unsigned, 
+		       unsigned, unsigned);
+void destroy_ffsb_thread (ffsb_thread_t *);
 
-/* owning thread group will start thread with this, thread runs */
-/* until *ft->checkval == ft->stopval */
-/* Yes this is not strictly synchronized, but that is okay for our */
-/* purposes, and it limits (IMO expensive) bus-locking */
+/* Owning thread group will start thread with this, thread runs until
+ * *ft->checkval == ft->stopval.  Yes this is not strictly
+ * synchronized, but that is okay for our purposes, and it limits (IMO
+ * expensive) bus-locking.
+ *
+ * pthread_create() is called by tg with this function as a parameter
+ * data is a (ffsb_thread_t*) 
+ */
+void * ft_run (void *);
 
-/* pthread_create() is called by tg with this function as a parameter */
-/* data is a (ffsb_thread_t*) */
-void*       ft_run( void * data );
+void ft_alter_bufsize (ffsb_thread_t *, unsigned);
+char * ft_getbuf (ffsb_thread_t *);
 
-void        ft_alter_bufsize(ffsb_thread_t *ft, unsigned bufsize);
-char*       ft_getbuf(ffsb_thread_t *ft);
+int ft_get_read_random (ffsb_thread_t *);
+uint32_t ft_get_read_size (ffsb_thread_t *);
+uint32_t ft_get_read_blocksize (ffsb_thread_t *);
 
-int         ft_get_read_random(ffsb_thread_t *ft);
-uint32_t   ft_get_read_size(ffsb_thread_t *ft);
-uint32_t   ft_get_read_blocksize(ffsb_thread_t *ft);
+int ft_get_write_random (ffsb_thread_t *);
+uint32_t ft_get_write_size (ffsb_thread_t *);
+uint32_t ft_get_write_blocksize (ffsb_thread_t *);
 
-int         ft_get_write_random(ffsb_thread_t *ft);
-uint32_t   ft_get_write_size(ffsb_thread_t *ft);
-uint32_t   ft_get_write_blocksize(ffsb_thread_t *ft);
+int ft_get_fsync_file (ffsb_thread_t *);
 
-int        ft_get_fsync_file(ffsb_thread_t *ft);
+randdata_t * ft_get_randdata (ffsb_thread_t *);
 
-randdata_t* ft_get_randdata(ffsb_thread_t *ft);
+void ft_incr_op(ffsb_thread_t *ft, unsigned opnum, unsigned increment);
 
-void        ft_incr_op(ffsb_thread_t *ft, unsigned opnum, unsigned increment);
+void ft_add_readbytes (ffsb_thread_t *, uint32_t);
+void ft_add_writebytes (ffsb_thread_t *, uint32_t);
 
-void        ft_add_readbytes(ffsb_thread_t *ft, uint32_t bytes );
-void        ft_add_writebytes(ffsb_thread_t *ft, uint32_t bytes );
+int ft_get_read_skip (ffsb_thread_t *);
+uint32_t ft_get_read_skipsize (ffsb_thread_t *);
 
-int         ft_get_read_skip(ffsb_thread_t *ft);
-uint32_t    ft_get_read_skipsize(ffsb_thread_t *ft);
+ffsb_op_results_t *ft_get_results (ffsb_thread_t *);
 
-ffsb_op_results_t *ft_get_results(ffsb_thread_t *ft);
-
-void        ft_set_statsc(ffsb_thread_t *ft, ffsb_statsc_t *fsc);
+void ft_set_statsc (ffsb_thread_t *, ffsb_statsc_t *);
 
 /* for these two, ft == NULL is OK */
-int         ft_needs_stats(ffsb_thread_t *ft, syscall_t s);
-void        ft_add_stat(ffsb_thread_t *ft, syscall_t sys, uint32_t val);
+int ft_needs_stats (ffsb_thread_t *, syscall_t);
+void ft_add_stat (ffsb_thread_t *, syscall_t, uint32_t);
 
-ffsb_statsd_t * ft_get_stats_data(ffsb_thread_t *ft);
-#endif
+ffsb_statsd_t * ft_get_stats_data (ffsb_thread_t *);
+
+#endif /* _FFSB_THREAD_H_ */
