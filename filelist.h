7d4d1ee66b3fb70a09453670da963999ee5d940c
Jose R. Santos <jsantos@gara>
2007-12-19 12:38:29 -0600

    Style changes to filelist.h
    Whitespace/comment fixes to filelist.h
    
    Signed-off-by: Jose R. Santos <jrs@us.ibm.com>

--- filelist.h
+++ filelist.h
@@ -17,6 +17,7 @@
  */
 #ifndef _FILELIST_H_
 #define _FILELIST_H_
+
 #include <pthread.h>
 #include "rand.h"
 #include "rwlock.h"
@@ -24,7 +25,7 @@
 #include "rbt.h"
 
 #define SUBDIRNAME_BASE "dir"
-#define FILENAME_BASE   "file"
+#define FILENAME_BASE "file"
 
 struct ffsb_file {
 	char *name;
@@ -35,83 +36,105 @@ struct ffsb_file {
 
 struct cirlist;
 
-/* Tree of ffsb_file structs and associated state info */
-/* struct must be locked during use */
+/* Tree of ffsb_file structs and associated state info struct must be
+ * locked during use.
+ */
 struct benchfiles {
-	/* the base directory in which all subdirs and files are created */
+	/* The base directory in which all subdirs and files are
+	 * created 
+	 */
 	char *basedir;
-	
-	/* the name to prepend to all directory and file names */
+
+	/* The name to prepend to all directory and file names */
 	char *basename;
 	uint32_t numsubdirs;
-	
-	/* files which currently exist on the filesystem */
-	struct red_black_tree* files;
-	
-	/* files which have been deleted, and whose numbers should be reused */
-	struct cirlist* holes;
 
-	/* this lock must be held while manipulating the structure */
+	/* Files which currently exist on the filesystem */
+	struct red_black_tree *files;
+
+	/* Files which have been deleted, and whose numbers should be
+	 * reused 
+	 */
+	struct cirlist *holes;
+
+	/* This lock must be held while manipulating the structure */
 	struct rwlock fileslock;
-	uint32_t listsize; /* sum size of nodes in files and holes */
+	uint32_t listsize; /* Sum size of nodes in files and holes */
 };
 
+/* Initializes the list, user must call this before anything else it
+ * will create the basedir and subdirs on the filesystem automatically
+ * if the builddirs arg. is nonzero
+ */
+void init_filelist (struct benchfiles *, char *, char *, uint32_t, int);
+void destroy_filelist (struct benchfiles *);
 
-/* Initializes the list, user must call this before anything else */
-/* it will create the basedir and subdirs on the filesystem automatically */
-/* if the builddirs arg. is nonzero */
-void init_filelist(struct benchfiles *bf, char * basedir, char * basename, uint32_t numsubdirs, int builddirs);
+/* Allocates a new file, adds to list, (write) locks it, and returns
+ * it.  This function also randomly selects a filename + path to
+ * assign to the new file.
+ *
+ * It first checks the "holes" list for any available filenames.
+ * Caller must ensure file is actually created on disk
+ */
+struct ffsb_file * add_file (struct benchfiles *b, uint64_t size,randdata_t *rd);
 
-void destroy_filelist(struct benchfiles *bf);
+/* Removes file from list, decrements listsize.
+ *
+ * File should be writer-locked before calling this function.
+ *
+ * This function does not unlock file after removal from list.
+ *
+ * Caller must ensure file is actually removed on disk.
+ *
+ * Caller must NOT free file->name and file, since oldfiles are being
+ * put into holes list.
+ */
+void remove_file (struct benchfiles *, struct ffsb_file *);
 
-/* allocates a new file, adds to list, (write) locks it, and returns it */
-/* This function also randomly selects a filename + path to assign to */
-/* the new file */
-/* it first checks the "holes" list for any available filenames */
-/* caller must ensure file is actually created on disk */
-struct ffsb_file * add_file(struct benchfiles *b, uint64_t size,randdata_t *rd);
+/* Picks a file at random, locks it for reading and returns it
+ * locked 
+ */
+struct ffsb_file * choose_file_reader (struct benchfiles *, randdata_t *);
 
-/* removes file from list, decrements listsize */
-/* file should be writer-locked before calling this function */
-/* this function does not unlock file after removal from list */
-/* caller must ensure file is actually removed on disk */
-/* caller must NOT free file->name and file, since oldfiles are being put into holes list */
-void remove_file(struct benchfiles *b, struct ffsb_file *file);
+/* Picks a file at random, locks it for writing and returns it
+ * locked 
+ */
+struct ffsb_file * choose_file_writer (struct benchfiles *, randdata_t *);
 
-/* picks a file at random, locks it for reading and returns it locked */
-struct ffsb_file * choose_file_reader(struct benchfiles *b, randdata_t *);
+/* changes the file->name of a file, file must be write locked
+ * it does not free the old file->name, so caller must keep a ref to it
+ * and free after the call
+ */
+void rename_file (struct ffsb_file *);
 
-/* picks a file at random, locks it for writing and returns it locked */
-struct ffsb_file * choose_file_writer(struct benchfiles *b, randdata_t *);
+void unlock_file_reader (struct ffsb_file *);
+void unlock_file_writer (struct ffsb_file *);
 
-/* changes the file->name of a file, file must be write locked  */
-/* it does not free the old file->name, so caller must keep a ref to it */
-/* and free after the call */
-void rename_file( struct ffsb_file * file);
+/* Uses SUBDIRNAME_BASE/FILENAME_BASE + bf->basename to validate a
+ * name returns a negative on invalid names, and the actual file
+ * number if valid
+ */
+int validate_filename (struct benchfiles *, char *);
+int validate_dirname (struct benchfiles *, char *);
 
-void unlock_file_reader(struct ffsb_file * file);
-void unlock_file_writer(struct ffsb_file * file);
+/* Function type which, does some validation of existing files
+ * currently only used by ffsb_fs stuff, returns 0 on success
+ */
+typedef int (*fl_validation_func_t)(struct benchfiles *, char *, void *);
 
-/* uses SUBDIRNAME_BASE/FILENAME_BASE + bf->basename to validate a name */
-/* returns a negative on invalid names, and the actual file number if valid */
-int validate_filename( struct benchfiles *bf, char *name);
-int validate_dirname ( struct benchfiles *bf, char *name);
+/* Provided for re-use of filesets.  Also runs the validation callback
+ * on each file/dir that is found, after verifying the name is
+ * conformant.  The fileset should be initialized with init_fileset()
+ * beforehand.
+ * Returns 0 on success
+ */
+int grab_old_fileset (struct benchfiles *, char *, fl_validation_func_t,
+		      void *);
 
-/* function type which, does some validation of existing files */
-/* currently only used by ffsb_fs stuff, returns 0 on success */
-typedef int (*fl_validation_func_t)(struct benchfiles*, char *, void* );
+/* Get the number of files */
+uint32_t get_listsize (struct benchfiles *);
 
-/* provided for re-use of filesets, and runs the validation callback on each */
-/* file/dir that is found, after verifying the name is conformant */
-/* the fileset should be initialized with init_fileset() beforehand */
-/* returns 0 on success */
-int grab_old_fileset( struct benchfiles *bf, char *basename, 
-		      fl_validation_func_t  vfunc, void * vfunc_data);
+/* Get the number of subdirectories */
+uint32_t get_numsubdirs (struct benchfiles *);
 
-/* get the number of files */
-uint32_t get_listsize(struct benchfiles *bf);
-
-/* get the number of subdirectories */
-uint32_t get_numsubdirs(struct benchfiles *bf);
-
-#endif
+#endif /* _FILELIST_H_ */
