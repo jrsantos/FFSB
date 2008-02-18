898190153a4dc0ca42c7747e55cc641c41be3fc3
Jose R. Santos <joser.santos@gmail.com>
2008-02-18 14:54:16 -0600

    [STYLE] Whitespace cleanup in rwlock.c
    Whitespace cleanup in rwlock.c

--- rwlock.c
+++ rwlock.c
@@ -23,14 +23,11 @@
 #include "config.h"
 #include "rwlock.h"
 
-
-
-
 void init_rwlock(struct rwlock *rw)
 {
 	rw->n_readers = 0;
-	pthread_mutex_init(&rw->plock,NULL);
-	pthread_cond_init(&rw->pcond,NULL);
+	pthread_mutex_init(&rw->plock, NULL);
+	pthread_cond_init(&rw->pcond, NULL);
 #ifdef RWDEBUG
 	rw->n_write_waiting = 0;
 	rw->n_read_waiting  = 0;
@@ -45,9 +42,8 @@ void rw_lock_read(struct rwlock *rw)
 #ifdef RWDEBUG
 	rw->n_read_waiting++;
 #endif
-	while( rw->n_readers < 0 ){
+	while (rw->n_readers < 0)
 		pthread_cond_wait(&rw->pcond, &rw->plock);
-	}
 	rw->n_readers++;
 #ifdef RWDEBUG
 	rw->n_read_waiting--;
@@ -61,9 +57,8 @@ void rw_lock_write(struct rwlock *rw)
 #ifdef RWDEBUG
 	rw->n_write_waiting++;
 #endif
-	while( rw->n_readers != 0 ){
+	while (rw->n_readers != 0)
 		pthread_cond_wait(&rw->pcond, &rw->plock);
-	}
 	rw->n_readers = -1;
 #ifdef RWDEBUG
 	rw->n_write_waiting--;
@@ -80,20 +75,20 @@ void rw_unlock_read(struct rwlock *rw)
 	pthread_cond_signal(&rw->pcond);
 	pthread_mutex_unlock(&rw->plock);
 }
+
 void rw_unlock_write(struct rwlock *rw)
 {
 	pthread_mutex_lock(&rw->plock);
 	rw->n_readers = 0 ;
 	pthread_cond_broadcast(&rw->pcond);
 	pthread_mutex_unlock(&rw->plock);
-
 }
 
 int rw_trylock_read(struct rwlock *rw)
 {
 	int ret = 1;
 	pthread_mutex_lock(&rw->plock);
-	if( rw->n_readers >= 0) {
+	if (rw->n_readers >= 0) {
 		rw->n_readers++;
 		ret = 0;
 	}
@@ -105,7 +100,7 @@ int rw_trylock_write(struct rwlock *rw)
 {
 	int ret = 1;
 	pthread_mutex_lock(&rw->plock);
-	if( rw->n_readers == 0) {
+	if (rw->n_readers == 0) {
 		ret = 0;
 		rw->n_readers = -1;
 	}
